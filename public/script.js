fb07dd08602a7d016b66ae61ffba0f138e620a14
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2024-03-31 20:56:06 +0200

    popup funtie werkt

--- public/script.js
+++ public/script.js
@@ -2,7 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const hamburgerButton = document.querySelector('.menu-button');
     const menu = document.querySelector('.menu-container');
     const closeIcon = document.querySelector('.menu-close-button');
-
+    // popup
+    const addButtonlist = document.querySelectorAll('.btn-add');
+    const popup = document.querySelector('.popup-container');
+    const closeButton = document.querySelector('.close-btn-popup');
+   
     hamburgerButton.addEventListener("click", toggleMenu);
     closeIcon.addEventListener("click", toggleMenu);
 
@@ -20,5 +24,25 @@ document.addEventListener("DOMContentLoaded", function() {
             menu.style.display = "none";
         }
     }
-    
+
+    // pop up
+    addButtonlist.forEach(function(addButton) {
+        addButton.addEventListener("click", togglePopup);
+    });
+
+    closeButton.addEventListener("click", togglePopup);
+
+    function togglePopup() {
+        popup.classList.toggle("showPopup");
+
+        if (popup.classList.contains("showPopup")) {
+  
+            popup.style.display = "none";
+        } else {
+          
+            popup.style.display = "block";
+        }
+
+    }
+
 });
